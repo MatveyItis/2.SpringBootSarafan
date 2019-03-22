4d8a5d2e94cddbac0b5497f27dbd9c9ea7be8065
matveymaletskov <maletskovitis@mail.ru>
2019-03-22 16:53:01 +0300

    add vue.js

---
+++ src/main/java/ru/itis/maletskov/SarafanApplication.java
@@ -0,0 +1,11 @@
+package ru.itis.maletskov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SarafanApplication.class, args);
+    }
+}
